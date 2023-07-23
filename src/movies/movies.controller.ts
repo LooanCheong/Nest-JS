6726de5783d69991564c477058774f72320f54ed
LooanCheong <jjyejun@naver.com>
2023-07-23 23:08:37 +0900

    More Decorators

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,23 +16,31 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get('/:id')
+  @Get('search')
+  search(@Query('year') year: string) {
+    return `Searching Year: ${year}`;
+  }
+
+  @Get(':id')
   getOne(@Param('id') id: string) {
     return `This will return one movie id: ${id}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData) {
+    return movieData;
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') id: string) {
     return `This will delete a movie id: ${id}`;
   }
 
-  @Patch('/:id')
-  patch(@Param('id') id: string) {
-    return `This will patch a movie id: ${id}`;
+  @Patch(':id')
+  patch(@Param('id') id: string, @Body() updateData) {
+    return {
+      updatedMovie: id,
+      ...updateData,
+    };
   }
 }
